a392fcab0a9eed1ba298b3ab3162ea3978da44fd
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-11 17:50:07 -0300

    ssl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,12 +8,12 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <link href="{{secure_asset('css/app.css')}}" rel="stylesheet">
     </head>
     <body>
         <div id="app" class="l-container">
         <app></app>
         </div>
-        <script src="{{asset('js/app.js')}}"></script>
+        <script src="{{secure_asset('js/app.js')}}"></script>
     </body>
 </html>
