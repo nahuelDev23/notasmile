29e93c52b19a52abe61780de01e5283cc1751c0b
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-11 18:05:51 -0300

    pwa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="{{secure_asset('css/app.css')}}" rel="stylesheet">
+        @laravelPWA
     </head>
     <body>
         <div id="app" class="l-container">
---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Tamare esa direccion no existe xd</h1>
+
+@endsection
