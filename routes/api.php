78cedb008cda9be4298c64b000bedbe289186153
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-14 12:45:32 -0300

    test buscador generico 2

--- routes/api.php
+++ routes/api.php
@@ -65,4 +65,4 @@ Route::delete('notas/eliminar/{notas}','NotasController@destroy')->name('notas.d
 Route::get('mostrar/notas/{id}','NotasController@mostrarNotas')->name('mostrar.notas');
 
 #test buscar
-Route::get('receta/buscar/','RecetaController@buscarReceta')->name('receta.buscar');
+Route::get('buscar/receta','RecetaController@buscarReceta')->name('buscar.receta');
